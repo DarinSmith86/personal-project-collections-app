f33167048e4c1e6df2a01e6ee3052651ea27addb
darin smith <darinsmith86@gmail.com>
2015-07-28 15:45:47 -0600

    updated app.js

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
+
 var users = require('./routes/users');
+var passport = require('passport');
+// var util = require('util');
+var InstagramStrategy = require('passport-instagram').Strategy;
+var INSTAGRAM_CLIENT_ID = "f45dd0e8e8c643be9037cee21b58f145";
+var INSTAGRAM_CLIENT_SECRET = "f4a2a4854e1d47c69a6baf43922c17e2";
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +28,105 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Use the InstagramStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Instagram
+//   profile), and invoke a callback with a user object.
+passport.use(new InstagramStrategy({
+    clientID: process.env.INSTAGRAM_CLIENT_ID,
+    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/instagram/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Instagram profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Instagram account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+// // configure Express
+// app.configure(function() {
+//   app.set('views', __dirname + '/views');
+//   app.set('view engine', 'ejs');
+//   app.use(express.logger());
+//   app.use(express.cookieParser());
+//   app.use(express.bodyParser());
+//   app.use(express.methodOverride());
+//   app.use(express.session({ secret: 'keyboard cat' }));
+//   // Initialize Passport!  Also use passport.session() middleware, to support
+//   // persistent login sessions (recommended).
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+//   app.use(app.router);
+//   app.use(express.static(__dirname + '/public'));
+// });
+
+
+app.get('/', function(req, res){
+
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// login on instagram API
+app.get('/auth/instagram',
+  passport.authenticate('instagram'));
+
+app.get('/auth/instagram/callback',
+  passport.authenticate('instagram', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log(req.user.displayName);
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.use(function (req, res, next) {
+   res.locals.user = req.user
+   next()
+})
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var bcrypt = require('bcryptjs');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/', function (req, res, next) {
+    gifs.find({}, function (err, docs) {
+        res.render('index', {gifs: docs});
+    })
 });
+
+router.post('/', function (req, res, next) {
+    gifs.insert(req.body);
+    res.redirect('/gifs');
+  });
 
 module.exports = router;
